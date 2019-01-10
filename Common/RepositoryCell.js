23477aa7a8de7c6586401c6038047523675e4ed9
TMMMMMS <1003022706@qq.com>
2019-01-10 20:31:47 +0800

    cell

--- Common/RepositoryCell.js
+++ Common/RepositoryCell.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import {Image, Text, View } from 'react-native';
 
 export default class RepositoryCell extends Component {
 
@@ -10,8 +10,20 @@ export default class RepositoryCell extends Component {
             <View style={{ padding: 10 }}>
                 <Text>{this.props.item.full_name}</Text>
                 <Text>{this.props.item.description}</Text>
-                <Text>{this.props.item.owner.avatar_url}</Text>
-                <Text>{this.props.item.stargazers_count}</Text>
+                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+                    <View style={{ flexDirection: 'row', alignContent: 'center' }}>
+                        <Text>Author:</Text>
+                        <Image
+                            style={{ height: 22, width: 22 }}
+                            source={{ uri: this.props.item.owner.avatar_url }}
+                        />
+                    </View>
+                    <View style={{ flexDirection: 'row', alignContent: 'center' }}>
+                        <Text>Stars:</Text>
+                        <Text>{this.props.item.stargazers_count}</Text>
+                    </View>
+                    {/* <Image style={{width:22, height:22}} source={require('ic_star')} /> */}
+                </View>
             </View>
         );
     }
